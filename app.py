45fb7d60d3b75957a918a199ddc5460df6e1096a
Kartik Shelke <kartikshelke2201@gmail.com>
2024-11-29 18:25:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ if st.button("Checkout"):
         st.warning("Your cart is empty!")
     else:
         # Show QR code or image for payment
-        st.image('your_qr_code_image.png', caption="Scan to Pay", use_column_width=True)
+        st.image('image.jpg', caption="Scan to Pay", use_column_width=True)
         st.success("Proceed to Payment!")
